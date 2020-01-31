ed57e8bf846b267ed41ce95d78c449aa547e7815
m.malkin <mmalkin@fun-box.ru>
2020-01-31 15:09:41 +0300

    version bump to 0.0.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CleanMixer.MixProject do
   def project do
     [
       app: :clean_mixer,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
