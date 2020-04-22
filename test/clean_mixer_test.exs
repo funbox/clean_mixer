241a34d495c14ab5bdae3b19aec294a932c51c38
m.malkin <mmalkin@fun-box.ru>
2020-04-22 12:35:09 +0300

    test for version in README added

--- test/clean_mixer_test.exs
+++ test/clean_mixer_test.exs
@@ -1,3 +1,21 @@
 defmodule CleanMixerTest do
   use ExUnit.Case
+
+  test "README install version check" do
+    app = :clean_mixer
+
+    app_version = "#{Application.spec(app, :vsn)}"
+    readme = File.read!("README.md")
+    [_, readme_versions] = Regex.run(~r/{:#{app}, "(.+)"/, readme)
+
+    assert Version.match?(
+             app_version,
+             readme_versions
+           ),
+           """
+           Install version constraint in README.md does not match to current app version.
+           Current App Version: #{app_version}
+           Readme Install Versions: #{readme_versions}
+           """
+  end
 end
