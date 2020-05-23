329e596826a14d0408b88741ef0716547e0ab9d5
mugimaru73 <Mugimaru@live.ru>
2020-05-23 22:43:48 +0400

    add an ability to customise mix clean_mixer.plantuml output path via --output-file param

--- test/mix/tasks/plantuml_test.exs
+++ test/mix/tasks/plantuml_test.exs
@@ -4,9 +4,13 @@ defmodule Mix.Tasks.CleanMixer.PlantumlTest do
   alias Mix.Tasks.CleanMixer.Plantuml
   import ExUnit.CaptureIO
 
-  setup do
-    File.rm(Plantuml.image_file_name())
-    File.rm(Plantuml.plantuml_file_name())
+  setup tags do
+    file_name = Map.get(tags, :output_file, Plantuml.default_file_name())
+
+    on_exit(fn ->
+      file_name |> Plantuml.image_file_name() |> File.rm()
+      file_name |> Plantuml.plantuml_file_name() |> File.rm()
+    end)
 
     :ok
   end
@@ -39,5 +43,15 @@ defmodule Mix.Tasks.CleanMixer.PlantumlTest do
 
     plant_uml = Plantuml.plantuml_file_name() |> File.read!()
     assert plant_uml =~ ~s(package "core_domain" {)
+  end
+
+  @tag output_file: "my_custom_clean_mixer_file-#{:rand.uniform()}"
+  test "it supports output file name customisation", %{output_file: output_file} do
+    capture_io(fn ->
+      Mix.Task.rerun("clean_mixer.plantuml", ["--output-file=#{output_file}"])
+    end) =~ output_file
+
+    plant_uml = Plantuml.plantuml_file_name(output_file) |> File.read!()
+    assert String.starts_with?(plant_uml, "@startuml")
   end
 end
